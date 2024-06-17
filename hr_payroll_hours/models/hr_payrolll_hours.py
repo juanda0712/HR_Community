9a314eeda1167f8fbe7f302ee6918268616381bf
Juan Rodriguez <juanrm071299@gmail.com>
2024-06-17 17:38:54 -0600

    new salary rules

--- hr_payroll_hours/models/hr_payrolll_hours.py
+++ hr_payroll_hours/models/hr_payrolll_hours.py
@@ -7,7 +7,7 @@ class HrPayrollHours(models.Model):
     description = fields.Char(string='Description', required=True)
     date = fields.Date(string='Date', required=True, default=fields.Date.today)
     hours = fields.Float(string='Hours', required=True, default=1)
-    type = fields.Selection([('extras', 'Extras'), ('double', 'Double')], string='Type', required=True)
+    type = fields.Selection([('extras', 'Extras'), ('double', 'Double'),('double', 'Double'),('holiday', 'Holiday'),('holiday_extra', 'Extra (In holiday)')], string='Type', required=True)
     employee_id = fields.Many2one(comodel_name='hr.employee', string='Employee',
                                   required=True,
                                   help="Choose Employee for Extended Hours")
